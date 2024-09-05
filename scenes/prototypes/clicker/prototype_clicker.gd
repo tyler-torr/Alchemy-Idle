a919bc6244492d00add09f23dc0d85ec60407578
tyler-torr <tylertorrella@gmail.com>
2024-09-04 20:54:34 -0700

    Added small comments & preparing to add _get_nodes()
    Coming back from vacation (Started the project right before going on vacation). Getting back into the swing of things, just adding simple comments.

--- scenes/prototypes/clicker/prototype_clicker.gd
+++ scenes/prototypes/clicker/prototype_clicker.gd
@@ -1,5 +1,4 @@
-class_name PrototypeClicker
-extends Control
+class_name PrototypeClicker extends Control
 
 
 @export var label : Label
@@ -8,17 +7,26 @@ var azurite : int = 0
 
 
 func _ready() -> void:
+	_get_nodes()
 	update_label_text()
 
 
+func _get_nodes() -> void:
+	label = get_node("%Label")
+	# button = get_node("%Button")
+
+
+# Creates 1 Azurite
 func create_azurite() -> void:
 	azurite += 1
 	update_label_text()
 
 
+# Updates text to say current Azurite amount
 func update_label_text() -> void:
 	label.text = "Azurite: %s" %azurite
 
 
+# On click, create Azurite
 func _on_button_pressed() -> void:
 	create_azurite()
